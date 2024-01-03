802755dacdb74029e8480319b7d028683d87fae6
nayesoum <104022857+nayesoum@users.noreply.github.com>
2024-01-03 19:41:10 +0100

    modification de l 'année dans le html et le js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var countDownDate = new Date("dec 31, 2023 00:00:00").getTime();
+var countDownDate = new Date("dec 31, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
